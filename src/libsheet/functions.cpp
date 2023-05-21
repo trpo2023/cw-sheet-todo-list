3b93d79590d60ca6e9fd7c6983688bee8f159dd9
Matvey Gotfrid <matveygot@yandex.ru>
2023-05-22 00:19:15 +0700

    Add 'window update' function

--- src/libsheet/functions.cpp
+++ src/libsheet/functions.cpp
@@ -85,3 +85,57 @@ void delete_task(GtkWidget* button, gpointer data)
 
     update_task_list();
 }
+
+void update_task_list()
+{
+    // Clear the existing task list
+    GList *children, *iter;
+    children = gtk_container_get_children(GTK_CONTAINER(task_list_box));
+    for (iter = children; iter != NULL; iter = g_list_next(iter))
+        gtk_widget_destroy(GTK_WIDGET(iter->data));
+    g_list_free(children);
+
+    // Read tasks from file and display outstanding tasks
+    std::ifstream file(TASK_FILE);
+    std::string line;
+    int index = 0;
+    while (std::getline(file, line)) {
+        if (line.empty())
+            continue;
+
+        int status = line[0] - '0';
+        std::string task_name = line.substr(2);
+
+        if (status == 0) {
+            GtkWidget* task_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 5);
+
+            GtkWidget* task_label = gtk_label_new(task_name.c_str());
+            gtk_box_pack_start(GTK_BOX(task_box), task_label, TRUE, TRUE, 0);
+
+            GtkWidget* task_button = gtk_button_new_with_label("✔");
+            g_signal_connect(
+                    task_button,
+                    "clicked",
+                    G_CALLBACK(update_task_status),
+                    GINT_TO_POINTER(index));
+            gtk_box_pack_start(GTK_BOX(task_box), task_button, FALSE, FALSE, 0);
+
+            GtkWidget* delete_button = gtk_button_new_with_label("✘");
+            g_signal_connect(
+                    delete_button,
+                    "clicked",
+                    G_CALLBACK(delete_task),
+                    GINT_TO_POINTER(index));
+            gtk_box_pack_start(
+                    GTK_BOX(task_box), delete_button, FALSE, FALSE, 0);
+
+            gtk_box_pack_start(
+                    GTK_BOX(task_list_box), task_box, FALSE, FALSE, 0);
+        }
+
+        index++;
+    }
+    file.close();
+
+    gtk_widget_show_all(task_list_box);
+}
