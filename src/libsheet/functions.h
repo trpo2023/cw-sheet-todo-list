7b3019205bbda43c6c0f1e11b44ecb0f234deaf5
Matvey Gotfrid <matveygot@yandex.ru>
2023-05-22 00:10:24 +0700

    Add 'delete function' define

--- src/libsheet/functions.h
+++ src/libsheet/functions.h
@@ -7,3 +7,5 @@ void update_task_list();
 void add_new_task(GtkWidget* widget, gpointer data);
 
 void update_task_status(GtkWidget* button, gpointer data);
+
+void delete_task(GtkWidget* button, gpointer data);
