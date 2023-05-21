0605870dd5bee127373ea3af953d1f7599e3b8b5
Matvey Gotfrid <matveygot@yandex.ru>
2023-05-21 23:38:54 +0700

    Move constants to main.cpp from functions.h

--- src/libsheet/functions.h
+++ src/libsheet/functions.h
@@ -4,10 +4,6 @@
 #include <gtk/gtk.h>
 #include <iostream>
 
-const std::string TASK_FILE = "tasks.txt";
-
-GtkWidget* task_list_box;
-
 void update_task_list();
 
 void add_new_task(GtkWidget* widget, gpointer data);
