a81213f435e736ff4d58a72a36b5b243ec05f523
FAnraqo <vs045920@gmail.com>
2023-05-24 03:00:39 +0700

    Add new tests in test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -7,18 +7,44 @@
 
 #include <libsheet/functions.h>
 
-CTEST(check_text_in_entry_true, test1)
+CTEST(checking_text_in_entry_true, test1)
 {
     std::string str = "A mojno 5 za kyrsovyu?";
     const int expected = 0;
-    const int result = checking_for_errors(str);
+    const int result = checking_entry(str);
     ASSERT_EQUAL(expected, result);
 }
 
-CTEST(check_text_in_entry_false, test2)
+CTEST(checking_text_in_entry_false, test2)
 {
     std::string str = "";
     const int expected = 1;
-    const int result = checking_for_errors(str);
+    const int result = checking_entry(str);
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(checking_text_in_entry_false_2, test3)
+{
+    std::string str = "Ошибка! Поле ввода пустое!";
+    const int expected = 1;
+    const int result = checking_entry(str);
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(checking_is_open_file_true, test4)
+{
+    std::string File_name = "tasks.txt";
+    std::string str = "Ошибка! Поле ввода пустое!";
+    const int expected = 0;
+    const int result = checking_is_open_file(File_name);
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(check_text_in_entry_false_, test5)
+{
+    std::string File_name = "false.txt";
+    std::string str = "Ошибка! Поле ввода пустое!";
+    const int expected = 1;
+    const int result = checking_is_open_file(File_name);
     ASSERT_EQUAL(expected, result);
 }
